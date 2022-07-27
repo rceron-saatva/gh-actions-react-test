7b86406a928bf32b4a0d47c07aa9a1567cc50f81
Rodrigo Ceron <rodrigo.c@saatvamattress.com>
2022-07-26 22:47:56 -0600

    fix on base path in vite config again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://rceron-saatva.github.io/',
+  base: 'https://rceron-saatva.github.io/gh-actions-react-test/',
   plugins: [react()]
 })
